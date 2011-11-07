09e7d9fd5bb0426a2c8264e90e88d6a60b567ab8
Martin Aspeli <optilude@gmail.com>
2011-11-07 20:58:46 +0000

    Add global info

---
+++ src/plone/app/debugtoolbar/browser/global.py
@@ -0,0 +1,39 @@
+from zope.viewlet.viewlet import ViewletBase
+
+class GlobalViewlet(ViewletBase):
+
+    def update(self):
+        from Globals import DevelopmentMode
+        self.debug = DevelopmentMode
+
+        import App.config
+        config = App.config.getConfiguration()
+
+        self.config = []
+        for name in sorted(config.getSectionAttributes()):
+
+            if name in (
+                'access', 'databases', 'eventlog', 'servers',
+            ):
+                continue
+
+            self.config.append((name, getattr(config, name)))
+        
+        self.servers = config.servers
+        
+        self.appInfo = self.context.getPhysicalRoot()['Control_Panel']
+
+        self.databases = []
+        paths = dict([(x[1], x[0],) for x in config.dbtab.mount_paths.items()])
+
+        for name in self.appInfo.Database.getDatabaseNames():
+            db = self.appInfo.Database[name]
+            dbtabEntry = config.dbtab.databases[name]
+
+            self.databases.append({
+                'name': name,
+                'location': db.db_name(),
+                'size': db.db_size(),
+                'cacheSize': dbtabEntry.getCacheSize(),
+                'mount': paths.get(name, None),
+            })
--- src/plone/app/debugtoolbar/browser/interfaces.py
+++ src/plone/app/debugtoolbar/browser/interfaces.py
@@ -1,4 +1,9 @@
+from zope.interface import Interface
 from zope.viewlet.interfaces import IViewletManager
+
+class IDebugToolbarLayer(Interface):
+    """Browser layer marker interface
+    """
 
 class IDebugToolbarViewletManager(IViewletManager):
     """Marker interface for the debug toolbar viewlet manager
