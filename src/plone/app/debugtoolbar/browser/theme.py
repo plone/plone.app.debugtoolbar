3c71e8d469e1f35fe118e1374e24fbe3ebbe980e
Martin Aspeli <optilude@gmail.com>
2011-11-10 01:11:13 +0000

    Theme panel

---
+++ src/plone/app/debugtoolbar/browser/theme.py
@@ -0,0 +1,31 @@
+from zope.interface import Interface
+from zope.interface import directlyProvidedBy
+from zope.component import queryUtility
+from zope.viewlet.viewlet import ViewletBase
+
+from Products.CMFCore.utils import getToolByName
+
+class ThemeViewlet(ViewletBase):
+
+    def update(self):
+        
+        skins = getToolByName(self.context, 'portal_skins')
+        url = getToolByName(self.context, 'portal_url')
+
+        defaultSkin = skins.getDefaultSkin()
+        requestVariable = skins.getRequestVarname()
+
+        self.portal_url = url()
+        self.portal = url.getPortalObject()
+
+        # CMF skin
+        self.themeName = self.request.get(requestVariable, defaultSkin)
+        self.skinPaths = dict(skins.getSkinPaths()).get(self.themeName, '').split(',')
+
+        # Browser layers
+        self.themeLayer = queryUtility(Interface, name=self.themeName)
+        self.layers = list(directlyProvidedBy(self.request).flattened())
+
+        # CSS/JS composition
+        self.css = getToolByName(self.context, 'portal_css')
+        self.js = getToolByName(self.context, 'portal_javascripts')
