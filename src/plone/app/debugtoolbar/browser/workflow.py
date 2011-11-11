1ba22bd4765395d355b00ae8d69739b7e4eff0fd
Martin Aspeli <optilude@gmail.com>
2011-11-11 01:00:04 +0000

    Workflow panel

---
+++ src/plone/app/debugtoolbar/browser/workflow.py
@@ -0,0 +1,44 @@
+from zope.viewlet.viewlet import ViewletBase
+
+from AccessControl import getSecurityManager
+from Products.CMFCore.utils import getToolByName
+
+class WorkflowViewlet(ViewletBase):
+
+    def update(self):
+        
+        # Workflow
+        workflow = getToolByName(self.context, 'portal_workflow')
+
+        self.workflowStatus = workflow.getInfoFor(self.context, 'review_state', None)
+
+        workflows = workflow.getWorkflowsFor(self.context)
+
+        self.workflowStates = [
+            workflow.getInfoFor(self.context, wf.state_var, None)
+            for wf in workflows
+        ]
+
+        self.workflowNames = [wf.getId() for wf in workflows]
+
+        # Permissions
+
+        self.roles = self.context.validRoles()
+        self.permissionInfo = []
+
+        sm = getSecurityManager()
+
+        for p in self.context.permission_settings():
+            name = p['name']
+            acquired = bool(p['acquire'])
+            granted = bool(sm.checkPermission(name, self.context))
+
+            roles = [r['selected'] == 'SELECTED'
+                        for r in self.context.rolesOfPermission(name)]
+            
+            self.permissionInfo.append({
+                'name': name,
+                'granted': granted,
+                'acquired': acquired,
+                'roles': roles,
+            })
