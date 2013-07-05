1309551feb76b78d5da6f3d58a7bb4849cbd7038
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-07-05 10:58:39 +0200

    Fix themelayer plugin
    Use IBrowserSkinType instead of generic  Interface which can
    return an real utility instead of an iface and broke the page
    rendering. Skin Layer must inherits from IBrowserSkinType

--- src/plone/app/debugtoolbar/browser/theme.py
+++ src/plone/app/debugtoolbar/browser/theme.py
@@ -2,8 +2,10 @@ from zope.interface import Interface
 from zope.interface import directlyProvidedBy
 from zope.component import queryUtility
 from zope.viewlet.viewlet import ViewletBase
+from zope.publisher.interfaces.browser import IBrowserSkinType
 
 from Products.CMFCore.utils import getToolByName
+
 
 class ThemeViewlet(ViewletBase):
 
@@ -23,7 +25,7 @@ class ThemeViewlet(ViewletBase):
         self.skinPaths = dict(skins.getSkinPaths()).get(self.themeName, '').split(',')
 
         # Browser layers
-        self.themeLayer = queryUtility(Interface, name=self.themeName)
+        self.themeLayer = queryUtility(IBrowserSkinType, name=self.themeName)
         self.layers = list(directlyProvidedBy(self.request).flattened())
 
         # CSS/JS composition
