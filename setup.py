ee9bbe3776154b4af3775850535b071c9bd4acb7
Maurits van Rees <maurits@vanrees.org>
2016-02-09 22:11:19 +0100

    Added classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-import os
 
 version = '1.1dev'
 
@@ -14,36 +13,40 @@ long_description = (
     open('CHANGES.rst').read()
     + '\n')
 
-setup(name='plone.app.debugtoolbar',
-      version=version,
-      description="Debug toolbar for Plone",
-      long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='plone.app.debugtoolbar',
+    version=version,
+    description="Debug toolbar for Plone",
+    long_description=long_description,
+    # Get more strings from
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.0",
         "Programming Language :: Python",
-        ],
-      keywords='plone debug toolbar',
-      author='Martin Aspeli',
-      author_email='optilude@gmail.com',
-      url='',
-      license='gpl',
-      packages=find_packages('src'),
-      package_dir = {'': 'src'},
-      namespace_packages=['plone', 'plone.app'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'Products.CMFPlone',
-          'zope.annotation',
-          'plone.transformchain',
-          'Paste',
-      ],
-      extras_require={'test': ['plone.app.testing']},
-      entry_points="""
-      # -*- Entry points: -*-
-  	  [z3c.autoinclude.plugin]
-  	  target = plone
-      """,
-      )
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='plone debug toolbar',
+    author='Martin Aspeli',
+    author_email='optilude@gmail.com',
+    url='',
+    license='gpl',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Products.CMFPlone',
+        'zope.annotation',
+        'plone.transformchain',
+        'Paste',
+    ],
+    extras_require={'test': ['plone.app.testing']},
+    entry_points="""
+    # -*- Entry points: -*-
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    )
