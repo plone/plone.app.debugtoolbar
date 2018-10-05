c7ad4741f0d4f71e5679740f6bc1126c15c98607
ale-rt <alessandro.pisa@gmail.com>
2018-10-05 15:25:03 +0200

    Fix test setup

--- src/plone/app/debugtoolbar/testing.py
+++ src/plone/app/debugtoolbar/testing.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
+from plone.app.testing import applyProfile
+from plone.app.testing import IntegrationTesting
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
-from plone.app.testing import IntegrationTesting
-from plone.app.testing import FunctionalTesting
-from plone.app.testing import applyProfile
 
-from zope.configuration import xmlconfig
 
 class PloneAppDebugtoolbar(PloneSandboxLayer):
 
@@ -14,15 +12,14 @@ class PloneAppDebugtoolbar(PloneSandboxLayer):
     def setUpZope(self, app, configurationContext):
         # Load ZCML for this package
         import plone.app.debugtoolbar
-        xmlconfig.file('configure.zcml',
-                       plone.app.debugtoolbar,
-                       context=configurationContext)
-
+        self.loadZCML(package=plone.app.debugtoolbar)
 
     def setUpPloneSite(self, portal):
         applyProfile(portal, 'plone.app.debugtoolbar:default')
 
+
 PLONE_APP_DEBUGTOOLBAR_FIXTURE = PloneAppDebugtoolbar()
-PLONE_APP_DEBUGTOOLBAR_INTEGRATION_TESTING = \
-    IntegrationTesting(bases=(PLONE_APP_DEBUGTOOLBAR_FIXTURE, ),
-                       name="PloneAppDebugtoolbar:Integration")
+PLONE_APP_DEBUGTOOLBAR_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(PLONE_APP_DEBUGTOOLBAR_FIXTURE, ),
+    name="PloneAppDebugtoolbar:Integration",
+)
