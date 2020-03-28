fb33d192f371f06bfab52e054fc259f40cf3be62
Maurits van Rees <maurits@vanrees.org>
2020-03-28 01:22:15 +0100

    Added Python 3.8 to classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     keywords='plone debug toolbar',
     author='Martin Aspeli',
